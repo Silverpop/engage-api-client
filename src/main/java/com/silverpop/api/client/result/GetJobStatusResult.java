2691c5a38dc022dfc7988af58f603d00b10c06b7
moritz <moritz.siuts@scoyo.com>
2014-02-25 14:47:49 +0100

    return an enum instead of a String for job status

--- src/main/java/com/silverpop/api/client/result/GetJobStatusResult.java
+++ src/main/java/com/silverpop/api/client/result/GetJobStatusResult.java
@@ -33,8 +33,15 @@ public class GetJobStatusResult implements ApiResult {
         return successElement;
     }
 
-    public String getJobStatus() {
-        return jobStatus;
+    public JobStatus getJobStatus() {
+        JobStatus status;
+        try {
+            status = JobStatus.valueOf(jobStatus);
+        } catch (IllegalArgumentException ex) {
+            // use the UNKNOWN value instead, Silverpop must have introduced a new valid value
+            status = JobStatus.UNKNOWN;
+        }
+        return status;
     }
 
     public String getJobDescription() {
@@ -45,4 +52,17 @@ public class GetJobStatusResult implements ApiResult {
         return parameters.getParameters();
     }
 
+
+    public static enum JobStatus {
+
+        WAITING,
+        RUNNING,
+        CANCELED,
+        ERROR,
+        COMPLETE,
+
+        /** Only used if Silverpop introduced a new value for JOB_STATUS */
+        UNKNOWN
+
+    }
 }
