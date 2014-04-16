c48f701d1e09e47c48f258fab23e601598f09ce3
vtsyms <vt77@nau.edu>
2014-04-15 19:32:29 -0700

    Improvement to logout.

--- src/main/java/com/silverpop/api/client/xmlapi/XmlApiClient.java
+++ src/main/java/com/silverpop/api/client/xmlapi/XmlApiClient.java
@@ -3,12 +3,11 @@ package com.silverpop.api.client.xmlapi;
 import com.silverpop.api.client.ApiClient;
 import com.silverpop.api.client.ApiCommand;
 import com.silverpop.api.client.command.LoginCommand;
-import com.silverpop.api.client.command.LogoutCommand;
 
 public class XmlApiClient extends ApiClient<XmlApiRequest>{
 
 	public XmlApiClient(String url, String username, String password) {
-		this(new XmlApiSession(url, new LoginCommand(username, password), new XmlApiClient(url)));
+		this(new XmlApiSession(url, new LoginCommand(username, password)));
 	}
 
 	public XmlApiClient(String url) {
