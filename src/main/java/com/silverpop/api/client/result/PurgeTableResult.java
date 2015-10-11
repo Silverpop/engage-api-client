e2288e231ec17d996c278263b90166f62d934313
lyonspe <lyonspe@us.ibm.com>
2015-10-11 12:30:17 -0400

    PurgeTable

--- src/main/java/com/silverpop/api/client/result/PurgeTableResult.java
+++ src/main/java/com/silverpop/api/client/result/PurgeTableResult.java
@@ -1,7 +1,31 @@
 package com.silverpop.api.client.result;
 
-/**
- * Created by PeterJLyons on 10/11/2015.
- */
-public class PurgeTableResult {
+import com.silverpop.api.client.ApiResult;
+
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+
+@XStreamAlias("RESULT")
+public class PurgeTableResult implements ApiResult {
+
+    @XStreamAlias("SUCCESS")
+    protected boolean success;
+
+    @XStreamAlias("JOB_ID")
+    protected long jobId;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public long getJobId() {
+        return jobId;
+    }
+
+    public void setJobId(long jobId) {
+        this.jobId = jobId;
+    }
 }
