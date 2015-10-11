82f1beea25a0f2de23f331f3e02df8676b9f6c71
lyonspe <lyonspe@us.ibm.com>
2015-10-11 12:29:56 -0400

    ListRecipientMailings

--- src/main/java/com/silverpop/api/client/command/ListRecipientMailingsCommand.java
+++ src/main/java/com/silverpop/api/client/command/ListRecipientMailingsCommand.java
@@ -1,7 +1,39 @@
 package com.silverpop.api.client.command;
 
-/**
- * Created by PeterJLyons on 10/11/2015.
- */
-public class ListRecipientMailingsCommand {
+import com.silverpop.api.client.ApiCommand;
+import com.silverpop.api.client.ApiResult;
+import com.silverpop.api.client.XmlApiProperties;
+import com.silverpop.api.client.command.elements.DeleteRowsElementType;
+import com.silverpop.api.client.result.ListRecipientMailingsResult;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+
+@XmlApiProperties("ListRecipientMailings")
+public class ListRecipientMailingsCommand implements ApiCommand {
+
+    @XStreamAlias("LIST_ID")
+    private long tableId;
+
+    @XStreamAlias("RECIPIENT_ID")
+    private long rows;
+
+    @Override
+    public Class<? extends ApiResult> getResultType() {
+        return ListRecipientMailingsResult.class;
+    }
+
+    public long getRows() {
+        return rows;
+    }
+
+    public void setRows(long rows) {
+        this.rows = rows;
+    }
+
+    public long getTableId() {
+        return tableId;
+    }
+
+    public void setTableId(long tableId) {
+        this.tableId = tableId;
+    }
 }
