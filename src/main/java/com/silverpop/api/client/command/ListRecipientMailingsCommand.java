90fd09c79a41b8b3b690cb333ed267f9e60dfb21
lyonspe <lyonspe@us.ibm.com>
2015-10-11 12:29:19 -0400

    PreviewMailing

---
+++ src/main/java/com/silverpop/api/client/command/ListRecipientMailingsCommand.java
@@ -0,0 +1,7 @@
+package com.silverpop.api.client.command;
+
+/**
+ * Created by PeterJLyons on 10/11/2015.
+ */
+public class ListRecipientMailingsCommand {
+}
---
+++ src/main/java/com/silverpop/api/client/result/PurgeTableResult.java
@@ -0,0 +1,7 @@
+package com.silverpop.api.client.result;
+
+/**
+ * Created by PeterJLyons on 10/11/2015.
+ */
+public class PurgeTableResult {
+}
